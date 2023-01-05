284e7de8cb93c9688789d447e8de451b221cca73
Alexey <yakor.alex@gmail.com>
2023-01-05 10:45:30 +0100

    update cli.py, brain_games/scripts/brain_games.py add module cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
 def main():
     print("Welcome to the Brain Games!")
-
+    cli.welcome_user()  # Вызов модуля приветствия (brain_games/cli.py)
 
 if __name__ == '__main__':
     main()
+
+
