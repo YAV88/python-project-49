bfd571e9f16136b823c31599fe3fa526e9010b1b
Alexey <yakor.alex@gmail.com>
2023-01-08 16:44:44 +0100

    rename file brain_games/games/game_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,11 +2,11 @@
 
 
 from brain_games.games_logic import games_start
-from brain_games.games import brain_even
+from brain_games.games import game_even
 
 
 def main():
-    games_start(brain_even)
+    games_start(game_even)
 
 
 if __name__ == '__main__':
