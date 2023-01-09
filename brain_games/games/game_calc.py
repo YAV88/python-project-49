05ca9431aeb6457c3328839db06a013a1417f4f4
Alexey <yakor.alex@gmail.com>
2023-01-09 09:17:42 +0100

    update brain_games/games/game_calc.py, brain_games/scripts/brain_calc.py

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,21 @@
+from random import randint, choice
+
+
+SPECIFICATION = "What is the result of the expression?"
+MIN_NUMBER = 1
+MAX_NUMBER = 10
+OPERATORS = ['+', '-', '*']
+
+
+def num_random():
+    num_one = randint(MIN_NUMBER, MAX_NUMBER)
+    num_two = randint(MIN_NUMBER, MAX_NUMBER)
+    sign = choice(OPERATORS)
+    question = f'{num_one} {sign} {num_two}'
+    if sign == '+':
+        correct_answer = num_one + num_two
+    elif sign == '-':
+        correct_answer = num_one - num_two
+    else:
+        correct_answer = num_one * num_two
+    return question, str(correct_answer)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games_logic import games_start
+from brain_games.games import game_calc
+
+
+def main():
+    games_start(game_calc)
+
+
+if __name__ == '__main__':
+    main()
