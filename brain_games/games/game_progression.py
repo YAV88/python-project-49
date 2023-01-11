e950ea65882a98e37653cca0fbc06cbc1ff48c2a
Alexey <yakor.alex@gmail.com>
2023-01-11 17:17:23 +0100

    update brain_games/games/game_progression.py, brain_games/scripts/brain_progression.py

---
+++ brain_games/games/game_progression.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+SPECIFICATION = 'What number is missing in the progression?'
+MIN_NUMBER = 1                      # Min step
+MAX_NUMBER = 5                      # Max step
+START_NUMBER = randint(1, 20)       # Initial number
+STOP_NUMBER = randint(60, 100)      # End number
+LENGTH = randint(5, 10)             # Amount of numbers
+
+
+def num_random():
+    step = randint(MIN_NUMBER, MAX_NUMBER)
+    progression = list(range(START_NUMBER, STOP_NUMBER, step))[:LENGTH]
+    index = randint(0, len(progression) - 1)
+    correct_answer = progression[index]
+    progression[index] = '..'
+
+    s = ''
+    for i in progression:
+        s += str(i) + ' '
+
+    return s.strip(), str(correct_answer)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game_logic import game_start
+from brain_games.games import game_progression
+
+
+def main():
+    game_start(game_progression)
+
+
+if __name__ == '__main__':
+    main()
