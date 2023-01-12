5aa9840e629560d9e4c8a22abe4627c230cb29e6
Alexey <yakor.alex@gmail.com>
2023-01-12 09:48:47 +0100

    update brain_games/games/game_prime.py

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,18 @@
+from random import randint
+
+SPECIFICATION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+MIN_NUMBER = 1      # Min number
+MAX_NUMBER = 100    # Max number
+
+
+def is_prime(num):
+    d = 2
+    while num % d != 0:
+        d += 1
+    return d == num
+
+
+def num_random():
+    num = randint(MIN_NUMBER, MAX_NUMBER)
+    correct_answer = 'yes' if is_prime(num) else 'no'
+    return num, correct_answer
