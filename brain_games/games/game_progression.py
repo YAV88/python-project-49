07e8433b949544fde14488a6c6186b4494589ab4
Alexey <yakor.alex@gmail.com>
2023-01-12 09:48:13 +0100

    update brain_games/games/game_progression.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,12 +1,11 @@
 from random import randint
 
-
 SPECIFICATION = 'What number is missing in the progression?'
-MIN_NUMBER = 1                      # Min step
-MAX_NUMBER = 5                      # Max step
-START_NUMBER = randint(1, 20)       # Initial number
-STOP_NUMBER = randint(60, 100)      # End number
-LENGTH = randint(5, 10)             # Amount of numbers
+MIN_NUMBER = 1  # Min step
+MAX_NUMBER = 5  # Max step
+START_NUMBER = randint(1, 20)  # Initial number
+STOP_NUMBER = randint(60, 100)  # End number
+LENGTH = randint(5, 10)  # Amount of numbers
 
 
 def num_random():
