ff4375a00cb95755bb3076d99ac5f3bb623d480d
Alexey <yakor.alex@gmail.com>
2023-01-12 09:47:16 +0100

    update brain_games/games/game_even.py

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,13 +1,15 @@
 from random import randint
 
 SPECIFICATION = 'Answer "yes" if the number is even, otherwise answer "no".'
+MIN_NUMBER = 1                      # Min number
+MAX_NUMBER = 100                    # Max number
 
 
-def even(num):  # Проверка четности
+def even(num):                      # Parity check
     return num % 2 == 0
 
 
-def num_random():  # Рандомное число
-    num = randint(1, 100)
+def num_random():                   # Random number
+    num = randint(MIN_NUMBER, MAX_NUMBER)
     correct_answer = 'yes' if even(num) else 'no'
     return num, correct_answer
