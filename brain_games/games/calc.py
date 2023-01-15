9f55778c5db2d1c0aa1459aa34b1d3bfd903f379
Alexey <yakor.alex@gmail.com>
2023-01-15 15:46:04 +0100

    update brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,15 +7,20 @@ MAX_NUMBER = 10
 OPERATORS = ['+', '-', '*']
 
 
-def num_random():
-    num_one = randint(MIN_NUMBER, MAX_NUMBER)
-    num_two = randint(MIN_NUMBER, MAX_NUMBER)
-    sign = choice(OPERATORS)
-    question = f'{num_one} {sign} {num_two}'
+def calculate(num_one, num_two, sign):
     if sign == '+':
         correct_answer = num_one + num_two
     elif sign == '-':
         correct_answer = num_one - num_two
     else:
         correct_answer = num_one * num_two
+    return correct_answer
+
+
+def num_random():
+    num_one = randint(MIN_NUMBER, MAX_NUMBER)
+    num_two = randint(MIN_NUMBER, MAX_NUMBER)
+    sign = choice(OPERATORS)
+    question = f'{num_one} {sign} {num_two}'
+    correct_answer = calculate(num_one, num_two, sign)
     return question, str(correct_answer)
