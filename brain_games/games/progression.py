f8db433a9964de68fc1f7aa4bd342e4806c085b6
Alexey <yakor.alex@gmail.com>
2023-01-17 11:00:23 +0100

    fix brain_games/games/progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,27 +2,23 @@ from random import randint
 
 
 DESCRIPTION = 'What number is missing in the progression?'
-MIN_NUMBER = 1  # Min step
-MAX_NUMBER = 5  # Max step
+MIN_NUMBER_STEP = 1  # Min step
+MAX_NUMBER_STEP = 5  # Max step
 START_NUMBER = randint(1, 20)  # Initial number
 STOP_NUMBER = randint(60, 100)  # End number
 LENGTH = 10  # Amount of numbers
 
 
-def progression_generation(num_1, num_2, num_3):
-    progression = list(range(num_1, num_2, num_3))[:LENGTH]
+def progression_generation(start, stop, step):
+    progression = list(range(start, stop, step))[:LENGTH]
     return progression
 
 
 def game_number_generation():
-    step = randint(MIN_NUMBER, MAX_NUMBER)
+    step = randint(MIN_NUMBER_STEP, MAX_NUMBER_STEP)
     progression = progression_generation(START_NUMBER, STOP_NUMBER, step)
     index = randint(0, len(progression) - 1)
     correct_answer = progression[index]
     progression[index] = '..'
-
-    s = ''
-    for i in progression:
-        s += str(i) + ' '
-
-    return s.strip(), str(correct_answer)
+    question = ' '.join(str(i) for i in progression)
+    return question, str(correct_answer)
