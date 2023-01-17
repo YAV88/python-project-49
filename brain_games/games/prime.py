992c16bb1da9f1adc34c74825211945efb74e478
Alexey <yakor.alex@gmail.com>
2023-01-17 16:55:35 +0100

    fix brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,14 +7,14 @@ MAX_NUMBER = 100
 
 def is_prime(num):
     divider = 2
+    if num >= 1:
+        return False
     while num % divider != 0:
         divider += 1
-        if 2 > num:
-            return False
     return divider == num
 
 
-def game_number_generation():
+def generate_round():
     num = randint(MIN_NUMBER, MAX_NUMBER)
     correct_answer = 'yes' if is_prime(num) else 'no'
-    return num, str(correct_answer)
+    return str(num), correct_answer
